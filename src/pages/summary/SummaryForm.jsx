9005eb87a73946cb3ffc5996fa5c8ad057c7ce6f
Alan M <alan.maldonado.lara1@gmail.com>
2022-01-07 16:31:26 -0600

    Test and implement popover functionality

--- src/pages/summary/SummaryForm.jsx
+++ src/pages/summary/SummaryForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Button, Form } from "react-bootstrap";
+import { Button, Form, Popover, OverlayTrigger } from "react-bootstrap";
 
 const SummaryForm = () => {
   const [tcChecked, setTcChecked] = useState(false);
@@ -8,26 +8,37 @@ const SummaryForm = () => {
     setTcChecked(e.target.checked);
   };
 
+  const popover = (
+    <Popover id="terms-popover">
+      <Popover.Body>No ice cream will be delivered</Popover.Body>
+    </Popover>
+  );
+
   const checkboxLabel = (
     <span>
-      I agree to the <span style={{ color: "blue" }}>Terms and Conditions</span>
+      I agree to the{" "}
+      <OverlayTrigger placement="bottom" overlay={popover}>
+        <span style={{ color: "blue" }}>Terms and Conditions</span>
+      </OverlayTrigger>
     </span>
   );
 
   return (
-    <Form>
-      <Form.Group controlId="terms-and-conditions">
-        <Form.Check
-          type="checkbox"
-          label={checkboxLabel}
-          onChange={handleTcChange}
-        />
-      </Form.Group>
+    <>
+      <Form>
+        <Form.Group controlId="terms-and-conditions">
+          <Form.Check
+            type="checkbox"
+            label={checkboxLabel}
+            onChange={handleTcChange}
+          />
+        </Form.Group>
 
-      <Button variant="primary" type="submit" disabled={!tcChecked}>
-        Place order
-      </Button>
-    </Form>
+        <Button variant="primary" type="submit" disabled={!tcChecked}>
+          Place order
+        </Button>
+      </Form>
+    </>
   );
 };
 
--- src/pages/summary/test/SummaryForm.test.jsx
+++ src/pages/summary/test/SummaryForm.test.jsx
@@ -1,8 +1,15 @@
-import { fireEvent, render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
 
 import SummaryForm from "./../SummaryForm";
 
-// LEARNING: The test component must be rendered IN EVERY test, not test suite (defined by describe)
+// LEARNING: The test component must be rendered IN EVERY test, not on top of describe statements
+// userEvents simulate user actions in a more realistic way than fireEvent
+// when something does not have a particular role, we use getByText()
 describe("Summary form has correct functionality", () => {
   test("button and checkbox initial conditions", () => {
     render(<SummaryForm />);
@@ -26,10 +33,43 @@ describe("Summary form has correct functionality", () => {
       name: /place order/i,
     });
 
-    fireEvent.click(termsCheckbox);
+    userEvent.click(termsCheckbox);
     expect(placeOrderButton).toBeEnabled();
 
-    fireEvent.click(termsCheckbox);
+    userEvent.click(termsCheckbox);
     expect(placeOrderButton).toBeDisabled();
   });
 });
+
+describe("terms and conditions popover functionality", () => {
+  test("popover is hidden by default", () => {
+    render(<SummaryForm />);
+
+    const nullPopover = screen.queryByText(/no ice cream will be delivered/i);
+    expect(nullPopover).not.toBeInTheDocument();
+  });
+
+  test("popover appears upon mouseover of checkbox label, then dissapears after mouse unhovers", async () => {
+    render(<SummaryForm />);
+
+    const termsCheckbox = screen.getByText(/terms and conditions/i);
+    userEvent.hover(termsCheckbox);
+
+    // getByText throws and error if there is no match, but it is a good practice
+    // to still write the assertion below for test readability.
+    const popover = screen.getByText(/no ice cream will be delivered/i);
+    expect(popover).toBeInTheDocument();
+
+    userEvent.unhover(termsCheckbox);
+    // queryByText returns null. It is useful for checking if an element exists,
+    // which the next assertion will make sure of.
+
+    await waitForElementToBeRemoved(() =>
+      screen.queryByText(/no ice cream will be delivered/i)
+    );
+    // waitForElementToBeRemoved already contains an assertion, we have to remove this
+    //expect(nullPopoverAfterInit).not.toBeInTheDocument();
+  });
+});
+
+// when using a function that contains a promise, we need to make it async
