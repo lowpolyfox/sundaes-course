a11aa68f539c6f1361781e815436760df16b61ff
Alan M <alan.maldonado.lara1@gmail.com>
2022-01-11 17:24:41 -0600

    tests for server error in order entry process

--- src/pages/entry/Options.jsx
+++ src/pages/entry/Options.jsx
@@ -1,18 +1,24 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import Row from "react-bootstrap/Row";
+import AlertBanner from "./../common/AlertBanner";
 import ScoopOption from "./ScoopOption";
 import ToppingOption from "./ToppingOption";
 
 export default function Options({ optionType }) {
   const [items, setItems] = useState([]);
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     axios
       .get(`http:localhost:3030/${optionType}`)
       .then((response) => setItems(response.data))
-      .catch((error) => console.log(error));
+      .catch((error) => setError(true))
   }, [optionType]);
+
+  if (error) {
+    return <AlertBanner />;
+  }
 
   const ItemComponent = optionType === "scoops" ? ScoopOption : ToppingOption;
 
